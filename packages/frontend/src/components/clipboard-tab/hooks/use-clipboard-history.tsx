3040ce832e90111a89d1b9d1b75a60fdafef19b8
plasticine <975036719@qq.com>
2024-05-04 23:03:52 +0800

    feat(frontend): support display clipboard history created time

--- packages/frontend/src/components/clipboard-tab/hooks/use-clipboard-history.tsx
+++ packages/frontend/src/components/clipboard-tab/hooks/use-clipboard-history.tsx
@@ -1,5 +1,6 @@
 import { Button, TableColumnsType } from 'antd'
 import { useMemo } from 'react'
+import dayjs from 'dayjs'
 
 import { ClipboardHistory } from '@filebrowser/shared'
 
@@ -30,6 +31,17 @@ export function useClipboardHistory() {
         dataIndex: 'content',
         key: 'content',
         render: (_, { content }) => <span style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>{content}</span>,
+      },
+      {
+        title: '时间',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        render: (_, { createdAt }) => {
+          const formatTime = dayjs(createdAt).format('YYYY-MM-DD HH:mm:ss')
+
+          return <span style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>{formatTime}</span>
+        },
       },
       {
         title: '操作',
