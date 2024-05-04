fdb8bfd4c9d1d62d40901e031d0a8a6e2fb94cbc
plasticine <975036719@qq.com>
2024-05-04 16:36:39 +0800

    feat(frontend): support share site

--- packages/frontend/src/App/index.tsx
+++ packages/frontend/src/App/index.tsx
@@ -1,5 +1,6 @@
-import { Tabs } from 'antd'
-import { memo, type FC } from 'react'
+import { ShareAltOutlined } from '@ant-design/icons'
+import { Button, Modal, QRCode, Tabs, TabsProps } from 'antd'
+import { memo, useCallback, useMemo, type FC } from 'react'
 
 import { useTabItems } from './hooks'
 import styles from './style.module.scss'
@@ -7,10 +8,31 @@ import styles from './style.module.scss'
 const App: FC = memo(() => {
   const { tabItems } = useTabItems()
 
+  const handleShareButtonClick = useCallback(() => {
+    Modal.info({
+      title: '二维码分享',
+      content: (
+        <div>
+          <QRCode value={window.location.href} />
+        </div>
+      ),
+    })
+  }, [])
+
+  const tabBarExtraContent = useMemo<TabsProps['tabBarExtraContent']>(() => {
+    return {
+      right: (
+        <Button icon={<ShareAltOutlined />} onClick={handleShareButtonClick}>
+          分享本站
+        </Button>
+      ),
+    }
+  }, [handleShareButtonClick])
+
   return (
     <div className={styles.app}>
       <div className={styles['app-wrapper']}>
-        <Tabs defaultActiveKey="file" items={tabItems} />
+        <Tabs defaultActiveKey="file" items={tabItems} tabBarExtraContent={tabBarExtraContent} />
       </div>
     </div>
   )
